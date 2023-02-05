6a5f5cee588d9dcf8f7d868d460905dc3879c2df
Nana <36996739+frostedana8@users.noreply.github.com>
2023-02-05 23:35:42 +0100

    recodificación (a medio hacer)
    para seguir trabajando en clase o algo

--- scripts/js.js
+++ scripts/js.js
@@ -6,12 +6,8 @@ const tienda = window.document.getElementById("tienda");
 const reset = window.document.getElementById("reset");
 const nombres = [];
 const botones = [];
-let datos = {
-    g: 0,
-    m1: 0,
-    m2: 0,
-    m3: 0
-}
+
+
 let mejoras = [{
     id: "m1",
     nombre: "Mejora",
@@ -32,129 +28,149 @@ let mejoras = [{
     multiplicador: 0
 }];
 
+
 inicializar();
 
 function inicializar() {
     if (localStorage.getItem("g") != 0) {
         cargar();
     }
-    dibujarTabla();
-    for (var i = 0; i < mejoras.length; i++) {
-        aux = i;
-        nombres[aux] = window.document.getElementById("nombre" + mejoras[aux].id);
-        botones[aux] = window.document.getElementById("precio" + mejoras[aux].id);
-    }
-    sincronizarMejoras();
+    // dibujarTabla();
+    // for (var i = 0; i < mejoras.length; i++) {
+    //     aux = i;
+    //     nombres[aux] = document.getElementById("nombre" + mejoras[aux].id);
+    //     botones[aux] = document.getElementById("precio" + mejoras[aux].id);
+    // }
     activarBotones();
     setInterval(function () {
-        actualizarVisual()
-    }, 20);
-    setInterval(function () {
-        actualizar();
+        actualizarGuita();
     }, 10);
     setInterval(function () {
         guardar();
     }, 5000);
 }
 
-boton.addEventListener("click", function () {
-    incremento();
-});
-
+//Esto hace que suba el dinero cuando picas
 function incremento() {
     guita += 1 + mejoras[0].unidades * mejoras[0].multiplicador;
 }
 
+
+//Esto hace que funcionen los botones, se hace al principio pero como es un addEventListener se queda hecho :3
 function activarBotones() {
-    botones[0].addEventListener("click", function () {
+    document.getElementById("m1").addEventListener("click", function () {
         if (guita >= (mejoras[0].precio + (1 * mejoras[0].unidades))) {
             guita -= (mejoras[0].precio + (1 * mejoras[0].unidades));
             mejoras[0].unidades++;
-            datos.m1++;
         }
     });
-    botones[1].addEventListener("click", function () {
+
+    document.getElementById("m2").addEventListener("click", function () {
         if (guita >= (mejoras[1].precio + (1 * mejoras[1].unidades))) {
             guita -= (mejoras[1].precio + (1 * mejoras[1].unidades));
             mejoras[1].unidades++;
-            datos.m2++;
         }
     });
-    botones[2].addEventListener("click", function () {
+
+    document.getElementById("m3").addEventListener("click", function () {
         if (guita >= (mejoras[2].precio * ((mejoras[2].unidades) + 1))) {
             guita -= (mejoras[2].precio * ((mejoras[2].unidades) + 1));
             mejoras[2].unidades++;
-            datos.m3++;
         }
     });
-    reset.addEventListener("click", function () {
-        reiniciar();
-    })
+
 }
 
-function actualizarVisual() {
-    cantidad.innerHTML = guita.toFixed(2) + '€';
-    for (var i = 0; i < mejoras.length; i++) {
-        nombres[i].innerHTML = mejoras[i].nombre + "(" + mejoras[i].unidades + ")";
-        if (i != 2)
-            botones[i].value = mejoras[i].precio + (1 * mejoras[i].unidades) + "€";
-        else
-            botones[i].value = mejoras[2].precio * ((mejoras[2].unidades) + 1)+ "€";
-    }
-}
 
-function actualizar() {
+
+//Esto hace correr la guita y dibujarla subiendo
+function actualizarGuita() {
     for (var i = 0; i < mejoras.length; i++) {
         guita = Number(guita) + Number(mejoras[i].multiplicador * mejoras[i].unidades);
     }
+    document.getElementById("cantidad").innerHTML = guita.toFixed(2) + "€"
 }
 
-function dibujarTabla() {
-    var t = "";
-    for (var i = 1; i <= 2; i++) {
-        t += "<tr>";
-        switch (i) {
-            case 1:
-                for (var j = 0; j < mejoras.length; j++) {
-                    t += "<td id=\"nombre" + mejoras[j].id + "\"></td>";
-                }
-                break;
-            case 2:
-                for (var j = 0; j < mejoras.length; j++) {
-                    t += "<td><input type=\"button\" id=\"precio" + mejoras[j].id + "\"></td>";
-                }
-                break;
-        }
-        t += "</tr>";
-    }
-    tienda.innerHTML = t;
-}
+
+
+
+
 
 function reiniciar() {
-    localStorage.setItem("g", 0);
-    localStorage.setItem("m1", 0);
-    localStorage.setItem("m2", 0);
-    localStorage.setItem("m3", 0);
-    location.reload()
+    guita = 0
+    mejoras[0].unidades = 0
+    mejoras[1].unidades = 0
+    mejoras[2].unidades = 0
+    guardar()
 }
+
 
 function guardar() {
     localStorage.setItem("g", guita);
-    localStorage.setItem("m1", datos.m1);
-    localStorage.setItem("m2", datos.m2);
-    localStorage.setItem("m3", datos.m3);
+    localStorage.setItem("m1", mejoras[0].unidades);
+    localStorage.setItem("m2", mejoras[1].unidades);
+    localStorage.setItem("m3", mejoras[2].unidades);
 }
+
 
 function cargar() {
-    datos.g = Number(localStorage.getItem("g"));
-    datos.m1 = Number(localStorage.getItem("m1"));
-    datos.m2 = Number(localStorage.getItem("m2"));
-    datos.m3 = Number(localStorage.getItem("m3"));
-    guita = datos.g;
+    guita = Number(localStorage.getItem("g"));
+    mejoras[0].unidades = Number(localStorage.getItem("m1"));
+    mejoras[1].unidades = Number(localStorage.getItem("m2"));
+    mejoras[2].unidades = Number(localStorage.getItem("m3"));  
 }
 
-function sincronizarMejoras() {
-    mejoras[0].unidades = Number(datos.m1);
-    mejoras[1].unidades = Number(datos.m2);
-    mejoras[2].unidades = Number(datos.m3);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function actualizarVisual() {
+//     cantidad.innerHTML = guita.toFixed(2) + '€';
+//     for (var i = 0; i < mejoras.length; i++) {
+//         nombres[i].innerHTML = mejoras[i].nombre + "(" + mejoras[i].unidades + ")";
+//         if (i != 2)
+//             botones[i].value = mejoras[i].precio + (1 * mejoras[i].unidades) + "€";
+//         else
+//             botones[i].value = mejoras[2].precio * ((mejoras[2].unidades) + 1)+ "€";
+//     }
+// }
+// function dibujarTabla() {
+//     var t = "";
+//     for (var i = 1; i <= 2; i++) {
+//         t += "<tr>";
+//         switch (i) {
+//             case 1:
+//                 for (var j = 0; j < mejoras.length; j++) {
+//                     t += "<td id=\"nombre" + mejoras[j].id + "\"></td>";
+//                 }
+//                 break;
+//             case 2:
+//                 for (var j = 0; j < mejoras.length; j++) {
+//                     t += "<td><input type=\"button\" id=\"precio" + mejoras[j].id + "\"></td>";
+//                 }
+//                 break;
+//         }
+//         t += "</tr>";
+//     }
+//     tienda.innerHTML = t;
+// }
